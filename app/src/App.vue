bf5670f8e604c49ab8ca36a56ca23a33ec0ed4af
jivko25 <jivkou@yahoo.com>
2023-10-11 20:00:31 +0300

    feat: replace default component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CardsWrapper/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardsWrapper from './components/CardsWrapper.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardsWrapper
   }
 }
 </script>
